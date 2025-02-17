bb40a72614cbae9fef9baaa7b99a4dfb468a555f
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-02-17 20:35:57 -0300

    feat(apps/api): create get profile route

---
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -0,0 +1,49 @@
+import type { FastifyInstance } from 'fastify';
+import type { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { z } from 'zod';
+
+import { prisma } from '@/lib/prisma';
+
+export async function getProfile(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/profile',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Get authenticated user profile.',
+        response: {
+          200: z.object({
+            id: z.string(),
+            name: z.string().nullable(),
+            email: z.string(),
+            avatarUrl: z.string().nullable(),
+          }),
+          404: z.object({
+            message: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { sub } = await request.jwtVerify<{ sub: string }>();
+
+      const user = await prisma.user.findUnique({
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          avatarUrl: true,
+        },
+        where: {
+          id: sub,
+        },
+      });
+
+      if (!user) {
+        return reply.status(404).send({ message: 'User does not exists.' });
+      }
+
+      return reply.status(200).send(user);
+    }
+  );
+}

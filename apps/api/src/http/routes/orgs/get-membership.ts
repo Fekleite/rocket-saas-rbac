e873dba19c3ad43303c14868951499d1ab6a28e7
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-02-21 22:43:58 -0300

    feat(apps/api): create get membership route

---
+++ apps/api/src/http/routes/orgs/get-membership.ts
@@ -0,0 +1,39 @@
+import { roleSchema } from '@rocket-saas/auth';
+import type { FastifyInstance } from 'fastify';
+import type { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { z } from 'zod';
+
+import { auth } from '@/http/middlewares/auth';
+
+export async function getMembership(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug/membership',
+      {
+        schema: {
+          tags: ['organizations'],
+          summary: 'Get user membership on organization',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              id: z.string(),
+              role: roleSchema,
+              organizationId: z.string(),
+              userId: z.string(),
+            }),
+          },
+        },
+      },
+      async (request) => {
+        const { slug } = request.params;
+        const { membership } = await request.getUserMembership(slug);
+
+        return membership;
+      }
+    );
+}

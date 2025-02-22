80f07b202189b982ce72ad9c7e5104bac6b68345
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-02-21 22:42:52 -0300

    feat(packages/auth): exposes role types

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -20,6 +20,8 @@ export * from './models/organization';
 export * from './models/project';
 export * from './models/user';
 
+export * from './roles';
+
 const appAbilitiesSchema = z.union([
   projectSubject,
   userSubject,

2c3bfd5710b5ff1a61ef1ed1eb13502b7844471a
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-03-06 20:52:22 -0300

    feat(apps/api): create reject invite route

---
+++ apps/api/src/http/routes/invites/reject-invite.ts
@@ -0,0 +1,62 @@
+import type { FastifyInstance } from 'fastify';
+import type { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { z } from 'zod';
+
+import { prisma } from '@/lib/prisma';
+import { auth } from '@/http/middlewares/auth';
+import { BadRequestError } from '../_errors/bad-request-error';
+
+export async function rejectInvite(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/invites/:inviteId/reject',
+      {
+        schema: {
+          tags: ['invites'],
+          summary: 'Reject an invite',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            inviteId: z.string().uuid(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { inviteId } = request.params;
+        const userId = await request.getCurrentUserId();
+
+        const invite = await prisma.invite.findUnique({
+          where: {
+            id: inviteId,
+          },
+          include: {},
+        });
+
+        if (!invite) {
+          throw new BadRequestError('Invite not found or expired.');
+        }
+
+        const user = await prisma.user.findUnique({ where: { id: userId } });
+
+        if (!user) {
+          throw new BadRequestError('User not found.');
+        }
+
+        if (invite.email !== user.email) {
+          throw new BadRequestError('This invite belongs to another user.');
+        }
+
+        await prisma.invite.delete({
+          where: {
+            id: inviteId,
+          },
+        });
+
+        return reply.status(204).send();
+      }
+    );
+}

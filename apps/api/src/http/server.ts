32171c022bac5677b639e2fa127af2b1ef1f71d3
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-03-06 19:29:56 -0300

    feat(apps/api): create get members route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -31,6 +31,7 @@ import { deleteProject } from './routes/projects/delete-project';
 import { getProject } from './routes/projects/get-project';
 import { getProjects } from './routes/projects/get-projects';
 import { updateProject } from './routes/projects/update-project';
+import { getMembers } from './routes/members/get-members';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -90,6 +91,9 @@ app.register(deleteProject);
 app.register(getProject);
 app.register(getProjects);
 app.register(updateProject);
+
+// Member
+app.register(getMembers);
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP server running!');

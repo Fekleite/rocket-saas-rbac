f25de22120c6808f77418224f387b1b6bdb9c00d
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-03-06 20:38:10 -0300

    feat(apps/api): remove unused auth method

--- apps/api/src/http/routes/invites/get-invite.ts
+++ apps/api/src/http/routes/invites/get-invite.ts
@@ -2,7 +2,6 @@ import type { FastifyInstance } from 'fastify';
 import type { ZodTypeProvider } from 'fastify-type-provider-zod';
 import { z } from 'zod';
 
-import { auth } from '@/http/middlewares/auth';
 import { prisma } from '@/lib/prisma';
 import { BadRequestError } from '../_errors/bad-request-error';
 import { roleSchema } from '@rocket-saas/auth';

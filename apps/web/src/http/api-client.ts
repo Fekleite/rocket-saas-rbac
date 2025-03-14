9e638d151be30b86fa3aabf1e4face67c46e3972
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-03-13 22:18:03 -0300

    feat(apps/web): create interceptor to add token on requests

--- apps/web/src/http/api-client.ts
+++ apps/web/src/http/api-client.ts
@@ -1,5 +1,26 @@
+import { getCookie } from 'cookies-next';
 import ky from 'ky';
 
 export const api = ky.create({
   prefixUrl: 'http://localhost:3333',
+  hooks: {
+    beforeRequest: [
+      async (request) => {
+        let token: string | undefined;
+
+        if (typeof window !== 'undefined') {
+          token = await getCookie('token');
+        } else {
+          const { cookies: nextCookies } = await import('next/headers');
+
+          const cookies = await nextCookies();
+          token = cookies.get('token')?.value;
+        }
+
+        if (token) {
+          request.headers.set('Authorization', `Bearer ${token}`);
+        }
+      },
+    ],
+  },
 });

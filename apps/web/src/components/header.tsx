ab152c9577f6ca8596ccaca770514c6ad45b2964
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-03-15 11:39:13 -0300

    feat(apps/web): create header component

---
+++ apps/web/src/components/header.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+
+import logoIcon from '@/assets/rocketseat-logo.svg';
+
+import { ProfileButton } from './profile-button';
+
+export function Header() {
+  return (
+    <header>
+      <div className="mx-auto flex max-w-[1280px] items-center justify-between">
+        <div className="flex items-center gap-3">
+          <Image src={logoIcon} className="h-6" alt="Rocketseat" />
+        </div>
+
+        <div className="flex items-center gap-4">
+          <ProfileButton />
+        </div>
+      </div>
+    </header>
+  );
+}

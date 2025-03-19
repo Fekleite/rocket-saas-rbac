9332dbc663baee230028429885079e9959445db6
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-03-18 23:13:03 -0300

    feat(apps/web): create organization switcher component

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image';
+import { Slash } from 'lucide-react';
 
 import logoIcon from '@/assets/rocketseat-logo.svg';
 
 import { ProfileButton } from './profile-button';
+import { OrganizationSwitcher } from './organization-switcher';
 
 export function Header() {
   return (
@@ -10,6 +12,10 @@ export function Header() {
       <div className="mx-auto flex max-w-[1280px] items-center justify-between">
         <div className="flex items-center gap-3">
           <Image src={logoIcon} className="h-6" alt="Rocketseat" />
+
+          <Slash className="text-border size-3 -rotate-[24deg]" />
+
+          <OrganizationSwitcher />
         </div>
 
         <div className="flex items-center gap-4">

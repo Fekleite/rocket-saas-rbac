929fa3e94b7c2151783e9b055c84a37356351cef
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-03-20 20:42:06 -0300

    refactor(packages/env): add next env variables

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 
 export const env = createEnv({
   server: {
-    PORT: z.coerce.number().default(3333),
+    API_PORT: z.coerce.number().default(3333),
 
     DATABASE_URL: z.string().url(),
 
@@ -14,9 +14,14 @@ export const env = createEnv({
     GITHUB_REDIRECT_URI: z.string().url(),
   },
   client: {},
-  shared: {},
+  shared: {
+    NEXT_PUBLIC_API_URL: z.string().url(),
+
+    NEXT_PUBLIC_GITHUB_SIGN_IN_ORIGIN: z.string().url(),
+    NEXT_PUBLIC_GITHUB_SIGN_IN_PATHNAME: z.string(),
+  },
   runtimeEnv: {
-    PORT: process.env.PORT,
+    API_PORT: process.env.PORT,
 
     DATABASE_URL: process.env.DATABASE_URL,
 
@@ -25,6 +30,13 @@ export const env = createEnv({
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
     GITHUB_REDIRECT_URI: process.env.GITHUB_REDIRECT_URI,
+
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+
+    NEXT_PUBLIC_GITHUB_SIGN_IN_ORIGIN:
+      process.env.NEXT_PUBLIC_GITHUB_SIGN_IN_ORIGIN,
+    NEXT_PUBLIC_GITHUB_SIGN_IN_PATHNAME:
+      process.env.NEXT_PUBLIC_GITHUB_SIGN_IN_PATHNAME,
   },
   emptyStringAsUndefined: true,
 });

badecacf6770a0252b7bfd5b117b3018c58c8a4c
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-03-20 19:39:02 -0300

    feat(apps/web): get permissions

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,7 +1,9 @@
 import { cookies as nextCookies } from 'next/headers';
 import { redirect } from 'next/navigation';
+import { defineAbilityFor } from '@rocket-saas/auth';
 
 import { getProfile } from '@/http/auth/get-profile';
+import { getMembership } from '@/http/organizations/get-membership';
 
 export async function isAuthenticated() {
   const cookies = await nextCookies();
@@ -26,4 +28,37 @@ export async function auth() {
   }
 
   redirect('/api/auth/sign-out');
+}
+
+export async function getCurrentOrg() {
+  const cookies = await nextCookies();
+
+  return cookies.get('org')?.value ?? null;
+}
+
+export async function getCurrentMembership() {
+  const org = await getCurrentOrg();
+
+  if (!org) {
+    return null;
+  }
+
+  const membership = await getMembership({ org });
+
+  return membership;
+}
+
+export async function ability() {
+  const membership = await getCurrentMembership();
+
+  if (!membership) {
+    return null;
+  }
+
+  const ability = defineAbilityFor({
+    id: membership.userId,
+    role: membership.role,
+  });
+
+  return ability;
 }
--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -3,10 +3,14 @@ import { Slash } from 'lucide-react';
 
 import logoIcon from '@/assets/rocketseat-logo.svg';
 
+import { ability } from '@/auth/auth';
+
 import { ProfileButton } from './profile-button';
 import { OrganizationSwitcher } from './organization-switcher';
 
-export function Header() {
+export async function Header() {
+  const permissions = await ability();
+
   return (
     <header className="py-4">
       <div className="mx-auto flex max-w-[1280px] items-center justify-between px-4">
@@ -16,6 +20,8 @@ export function Header() {
           <Slash className="text-border size-3 -rotate-[24deg]" />
 
           <OrganizationSwitcher />
+
+          {permissions?.can('get', 'Project') && 'Projetos'}
         </div>
 
         <div className="flex items-center gap-4">
